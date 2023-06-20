7c28122d9b5f5eaadc0c668e3bbd505f0487a952
Mehmet Selçuk Güler <gercekselcukguler@gmail.com>
2023-06-20 19:28:43 +0300

    Declare data object for projects into main.js file

--- main.js
+++ main.js
@@ -7,6 +7,69 @@ const menuElements = document.querySelector('#nav-list')
 const closeIcon = document.querySelector('#close-Icon')
 const body = document.getElementsByTagName('body')[0]
 
+const projectsData = [
+  {
+    title: 'Multi-Post Stories Gain+Glory',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s." ,
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    button: 'See Project',
+    image: 'images/pr01img.png',
+    link: 'https://s2jack.github.io/',
+    github: 'https://github.com/s2jack/create-portfolio-website1',
+  },
+  {
+    title: 'Multi-Post Stories Gain+Glory',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s." ,
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    button: 'See Project',
+    image: 'images/pr02img.png',
+    link: 'https://s2jack.github.io/',
+    github: 'https://github.com/s2jack/create-portfolio-website1',
+  },
+  {
+    title: 'Multi-Post Stories Gain+Glory',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s." ,
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    button: 'See Project',
+    image: 'images/pr03img.png',
+    link: 'https://s2jack.github.io/',
+    github: 'https://github.com/s2jack/create-portfolio-website1',
+  },
+  {
+    title: 'Multi-Post Stories Gain+Glory',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s." ,
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    button: 'See Project',
+    image: 'images/pr04img.png',
+    link: 'https://s2jack.github.io/',
+    github: 'https://github.com/s2jack/create-portfolio-website1',
+  },
+  {
+    title: 'Multi-Post Stories Gain+Glory',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s." ,
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    button: 'See Project',
+    image: 'images/pr05img.png',
+    link: 'https://s2jack.github.io/',
+    github: 'https://github.com/s2jack/create-portfolio-website1',
+  },
+  {
+    title: 'Multi-Post Stories Gain+Glory',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s." ,
+    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    button: 'See Project',
+    image: 'images/pr06img.png',
+    link: 'https://s2jack.github.io/',
+    github: 'https://github.com/s2jack/create-portfolio-website1',
+  },
+];
+
 const menuOpen = () => {
   hamIcon.style.display = 'none'
   mobMenuCont.style = 'align-items: unset; position: sticky; top: 0; background-color: #fff; width: 100vw; height: 100vh;'
