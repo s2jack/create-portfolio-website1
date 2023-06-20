edd4f0d536d14c0fcfab4f5f096359089b5e5d0b
Mehmet Selçuk Güler <gercekselcukguler@gmail.com>
2023-06-20 21:02:07 +0300

    Create works section dynamically
    Create a new function to load whole works section into main.js
    Call loadfunction
    Automate getting info from database for every cards
    Fix style errors
    Delete all works section's inner elements

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ const projectsData = [
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s." ,
     technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-    button: 'See Project',
     image: 'images/pr01img.png',
     link: 'https://s2jack.github.io/',
     github: 'https://github.com/s2jack/create-portfolio-website1',
@@ -24,7 +23,6 @@ const projectsData = [
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s." ,
     technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-    button: 'See Project',
     image: 'images/pr02img.png',
     link: 'https://s2jack.github.io/',
     github: 'https://github.com/s2jack/create-portfolio-website1',
@@ -34,7 +32,6 @@ const projectsData = [
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s." ,
     technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-    button: 'See Project',
     image: 'images/pr03img.png',
     link: 'https://s2jack.github.io/',
     github: 'https://github.com/s2jack/create-portfolio-website1',
@@ -44,7 +41,6 @@ const projectsData = [
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s." ,
     technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-    button: 'See Project',
     image: 'images/pr04img.png',
     link: 'https://s2jack.github.io/',
     github: 'https://github.com/s2jack/create-portfolio-website1',
@@ -54,7 +50,6 @@ const projectsData = [
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s." ,
     technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-    button: 'See Project',
     image: 'images/pr05img.png',
     link: 'https://s2jack.github.io/',
     github: 'https://github.com/s2jack/create-portfolio-website1',
@@ -64,7 +59,6 @@ const projectsData = [
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s." ,
     technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
-    button: 'See Project',
     image: 'images/pr06img.png',
     link: 'https://s2jack.github.io/',
     github: 'https://github.com/s2jack/create-portfolio-website1',
@@ -90,6 +84,64 @@ const menuClose = () => {
   closeIcon.style = ''
   menuElements.style = ''
 }
+
+const loadWorksSection = (worksList) => {
+  /* create section heading and main container that contains all of the work cards */
+  const sectionHeadingContainer = document.createElement('div');
+  sectionHeadingContainer.id = 'works-head-cont';
+  worksSection.appendChild(sectionHeadingContainer);
+  const sectionHeading = document.createElement('h2');
+  sectionHeading.className = 'black-text section-header';
+  sectionHeading.id = 'works-header';
+  sectionHeading.innerText = 'My Recent Works';
+  sectionHeadingContainer.appendChild(sectionHeading);
+  const cardsContainer = document.createElement('div');
+  cardsContainer.id = 'cards-cont';
+  worksSection.appendChild(cardsContainer);
+
+  for (let i = 0; i < worksList.length; i++) {
+    /* create card container with className and id */
+    const card = document.createElement('div');
+    const order = i + 1;
+    card.className = 'card';
+    card.id= 'card' + order.toString();  /* every card automatically takes id with order number. Exp: card1 for first card container */
+    cardsContainer.appendChild(card);
+    /* create card image and project info elements*/
+    const prImg = document.createElement('div');
+    prImg.className = 'project-img';
+    prImg.id = 'prImg' + order.toString();
+    card.appendChild(prImg);
+    const prInfo = document.createElement('div');
+    prInfo.className = 'project-info';
+    prInfo.id = 'project' + order.toString();
+    card.appendChild(prInfo);
+    /* create project title, technologies and details button */
+    const prName = document.createElement('h3');
+    prName.className = 'project-name';
+    prName.innerText = worksList[i]["title"];
+    prInfo.appendChild(prName);
+    const prTags = document.createElement('ul');
+    prTags.className = 'pr-tags-ul';
+      /* create list elements as same as with the object property in the data*/
+    for (let j = 0; j < worksList[i]['technologies'].length; j++) {
+      const tagElement = document.createElement('li');
+      tagElement.className = 'pr-tag';
+      tagElement.innerText = worksList[i]['technologies'][j];
+      prTags.appendChild(tagElement);
+    };
+    prInfo.appendChild(prTags);
+    const detailsBtnContainer = document.createElement('div');
+    detailsBtnContainer.id = 'btn-cont';
+    prInfo.appendChild(detailsBtnContainer);
+    const detailsBtn = document.createElement('button');
+    detailsBtn.id = 'pr-detail-btn';
+    detailsBtn.className = 'green-btn';
+    detailsBtn.innerText = 'See project';
+    detailsBtnContainer.appendChild(detailsBtn);
+  }
+}
+
+loadWorksSection(projectsData);
 
 hamIcon.addEventListener('click', menuOpen)
 menuElements.addEventListener('click', menuClose)
